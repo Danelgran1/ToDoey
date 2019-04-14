5f26dccaa2e112642771d0bd19238234e9247f57
Dan Munoz <dmunoz1@comcast.net>
2019-04-14 09:56:44 -0400

    Convert to MVC design pattern

---
+++ ToDoey/DataModel/item.swift
@@ -0,0 +1,13 @@
+//
+//  item.swift
+//  ToDoey
+//
+//  Created by Daniel Munoz on 4/14/19.
+//  Copyright © 2019 Daniel Munoz. All rights reserved.
+//
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
